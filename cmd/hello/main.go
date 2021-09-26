8ba4a4274d992b4ba2bb3433e9d112868ae8b83b
Vui Lam <vui@vmware.com>
2021-09-25 17:14:02 -0700

    Add dummy go project

---
+++ cmd/hello/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zerologr"
+	//"github.com/hn8/zerologr"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+	zerologr.NameFieldName = "logger"
+	zerologr.NameSeparator = "/"
+
+	zl := zerolog.New(os.Stderr)
+	var log logr.Logger = zerologr.New(&zl)
+
+	log.Info("Logr in action!", "the answer", 42)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vuil/testworkflow
+
+go 1.16
+
+require (
+	github.com/go-logr/logr v1.1.0
+	github.com/go-logr/zerologr v1.1.3
+	github.com/rs/zerolog v1.25.0
+)
